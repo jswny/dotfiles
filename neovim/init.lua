d5f3b57beb70b5182a4a6a697e8b40c06228c4b5
Joe Sweeney <joe@j1.io>
2023-02-11 16:37:04 -0500

    Lazy packaging

--- neovim/init.lua
+++ neovim/init.lua
@@ -3,127 +3,12 @@ vim.opt.termguicolors = true
 vim.opt.tabstop = 2
 vim.opt.signcolumn = "yes"
 vim.opt.number = true
+vim.opt.foldenable = false
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
-
--- Packaging
-local packer_compile_path = vim.fn.resolve(vim.fn.stdpath("data") .. "/site/packer_compiled.lua")
-
-vim.cmd("source " .. packer_compile_path)
-
-require("packer").startup({
-  function(use)
-    use("wbthomason/packer.nvim")
-    use({
-      "nvim-treesitter/nvim-treesitter",
-      run = function()
-        require("nvim-treesitter.install").update({ with_sync = true })
-      end,
-    })
-    use("neovim/nvim-lspconfig")
-    use("williamboman/mason.nvim")
-    use("williamboman/mason-lspconfig.nvim")
-    use("hrsh7th/cmp-nvim-lsp")
-    use("hrsh7th/cmp-buffer")
-    use("hrsh7th/cmp-path")
-    use("hrsh7th/cmp-cmdline")
-    use("hrsh7th/nvim-cmp")
-    use("L3MON4D3/LuaSnip")
-    use("saadparwaiz1/cmp_luasnip")
-    use("nvim-tree/nvim-web-devicons")
-    use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" })
-    use({
-      "nvim-telescope/telescope.nvim",
-      tag = "0.1.1",
-      requires = { { "nvim-lua/plenary.nvim" } },
-    })
-    use("folke/tokyonight.nvim")
-    use("nvim-lualine/lualine.nvim")
-    use({
-      "folke/trouble.nvim",
-      requires = "nvim-tree/nvim-web-devicons",
-    })
-    use({
-      "jose-elias-alvarez/null-ls.nvim",
-      requires = "nvim-lua/plenary.nvim",
-    })
-    use("lukas-reineke/indent-blankline.nvim")
-    use("numToStr/Comment.nvim")
-    use("lewis6991/gitsigns.nvim")
-    use("windwp/nvim-autopairs")
-    use({
-      "nvim-tree/nvim-tree.lua",
-      requires = "nvim-tree/nvim-web-devicons",
-      tag = "nightly",
-    })
-    use({
-      "glepnir/dashboard-nvim",
-      event = "VimEnter",
-      requires = "nvim-tree/nvim-web-devicons",
-      config = function()
-        require("dashboard").setup()
-      end,
-    })
-    use({
-      "utilyre/barbecue.nvim",
-      tag = "*",
-      requires = {
-        "SmiteshP/nvim-navic",
-        "nvim-tree/nvim-web-devicons",
-      },
-    })
-    use({ "akinsho/bufferline.nvim", tag = "v3.*", requires = "nvim-tree/nvim-web-devicons" })
-    use({
-      "ggandor/leap.nvim",
-      requires = "tpope/vim-repeat",
-      config = function()
-        require("leap").add_default_mappings()
-      end,
-    })
-  end,
-  config = {
-    compile_path = packer_compile_path,
-    display = {
-      open_fn = function()
-        return require("packer.util").float({ border = "single" })
-      end,
-    },
-  },
-})
-
-require("mason").setup()
-require("mason-lspconfig").setup({
-  automatic_installation = true,
-})
-require("Comment").setup()
-require("gitsigns").setup()
-require("nvim-autopairs").setup()
-require("nvim-tree").setup()
-require("bufferline").setup({})
-
--- Treesitter
-require("nvim-treesitter.configs").setup({
-  ensure_installed = "all",
-
-  sync_install = false,
-
-  auto_install = true,
-
-  highlight = {
-    enable = true,
-  },
-})
-vim.opt.foldmethod = "expr"
-vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
-vim.opt.foldenable = false
+vim.g.mapleader = "\\"
 
 -- LSP
-local opts = { noremap = true, silent = true }
-vim.keymap.set("n", "<space>e", vim.diagnostic.open_float, opts)
-vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
-vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
-vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist, opts)
-
 local lsp_formatting_augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 local on_attach = function(client, bufnr)
@@ -158,172 +43,306 @@ local on_attach = function(client, bufnr)
   end
 end
 
-local lspconfig = require("lspconfig")
-local capabilities = require("cmp_nvim_lsp").default_capabilities()
+-- Packaging
+local lazy_path = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
-local lsp_servers = { "elixirls", "sumneko_lua" }
-for _, lsp in ipairs(lsp_servers) do
-  lspconfig[lsp].setup({
-    on_attach = on_attach,
-    capabilities = capabilities,
+if not vim.loop.fs_stat(lazy_path) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazy_path,
   })
 end
 
-lspconfig["sumneko_lua"].setup({
-  settings = {
-    Lua = {
-      diagnostics = {
-        globals = { "vim" },
+vim.opt.rtp:prepend(lazy_path)
+
+local plugins = {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = "all",
+        sync_install = false,
+        auto_install = true,
+        highlight = {
+          enable = true,
+        },
+        indent = {
+          enable = false,
+        },
+      })
+
+      vim.opt.foldmethod = "expr"
+      vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+      "hrsh7th/cmp-nvim-lsp",
+    },
+    config = function()
+      local opts = { noremap = true, silent = true }
+      vim.keymap.set("n", "<space>e", vim.diagnostic.open_float, opts)
+      vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+      vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+      vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist, opts)
+
+      require("mason").setup()
+      require("mason-lspconfig").setup({ automatic_installation = true })
+
+      local lspconfig = require("lspconfig")
+      local cmp_capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+      local lsp_servers = { "elixirls", "sumneko_lua" }
+      for _, lsp in ipairs(lsp_servers) do
+        lspconfig[lsp].setup({
+          on_attach = on_attach,
+          capabilities = cmp_capabilities,
+        })
+      end
+
+      lspconfig["sumneko_lua"].setup({
+        settings = {
+          Lua = {
+            diagnostics = {
+              globals = { "vim" },
+            },
+          },
+        },
+      })
+    end,
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-cmdline",
+    },
+    config = function()
+      local cmp = require("cmp")
+      local luasnip = require("luasnip")
+
+      vim.opt.completeopt = { "menu", "menuone", "noselect" }
+
+      local has_words_before = function()
+        unpack = unpack or table.unpack
+        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+      end
+
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            require("luasnip").lsp_expand(args.body)
+          end,
+        },
+        window = {
+          completion = cmp.config.window.bordered(),
+          documentation = cmp.config.window.bordered(),
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-e>"] = cmp.mapping.abort(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+              luasnip.expand_or_jump()
+            elseif has_words_before() then
+              cmp.complete()
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+
+          ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+        }, {
+          { name = "buffer" },
+        }),
+      })
+
+      cmp.setup.filetype("gitcommit", {
+        sources = cmp.config.sources({
+          { name = "cmp_git" },
+        }, {
+          { name = "buffer" },
+        }),
+      })
+
+      cmp.setup.cmdline({ "/", "?" }, {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = "buffer" },
+        },
+      })
+
+      cmp.setup.cmdline(":", {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources({
+          { name = "path" },
+        }, {
+          { name = "cmdline" },
+        }),
+      })
+    end,
+  },
+  "L3MON4D3/LuaSnip",
+  "saadparwaiz1/cmp_luasnip",
+  {
+    "nvim-telescope/telescope.nvim",
+    version = "0.1.1",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = "make",
+      },
+    },
+    keys = {
+      { "<leader>ff", "<cmd>Telescope find_files<cr>" },
+      { "<leader>fg", "<cmd>Telescope live_grep<cr>" },
+      { "<leader>fb", "<cmd>Telescope buffers<cr>" },
+      { "<leader>fh", "<cmd>Telescope help_tags<cr>" },
+      { "<leader>fc", "<cmd>Telescope current_buffer_fuzzy_find<cr>" },
+    },
+    config = function()
+      require("telescope").load_extension("fzf")
+    end,
+  },
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd([[colorscheme tokyonight-night]])
+    end,
+  },
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = {
+      options = {
+        theme = "tokyonight",
       },
     },
   },
-})
+  {
+    "folke/trouble.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      local lsp_signs = {
+        Error = " ",
+        Warn = " ",
+        Hint = " ",
+        Info = " ",
+        Other = "﫠",
+      }
 
--- Completion
-local cmp = require("cmp")
-local luasnip = require("luasnip")
+      for type, icon in pairs(lsp_signs) do
+        local hl = "DiagnosticSign" .. type
+        vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+      end
 
-vim.opt.completeopt = { "menu", "menuone", "noselect" }
-
-local has_words_before = function()
-  unpack = unpack or table.unpack
-  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-end
-
-cmp.setup({
-  snippet = {
-    expand = function(args)
-      require("luasnip").lsp_expand(args.body)
+      vim.keymap.set("n", "<leader>xx", "<cmd>TroubleToggle<cr>", { silent = true, noremap = true })
+      vim.keymap.set(
+        "n",
+        "<leader>xw",
+        "<cmd>TroubleToggle workspace_diagnostics<cr>",
+        { silent = true, noremap = true }
+      )
+      vim.keymap.set(
+        "n",
+        "<leader>xd",
+        "<cmd>TroubleToggle document_diagnostics<cr>",
+        { silent = true, noremap = true }
+      )
+      vim.keymap.set("n", "<leader>xl", "<cmd>TroubleToggle loclist<cr>", { silent = true, noremap = true })
+      vim.keymap.set("n", "<leader>xq", "<cmd>TroubleToggle quickfix<cr>", { silent = true, noremap = true })
+      vim.keymap.set("n", "gR", "<cmd>TroubleToggle lsp_references<cr>", { silent = true, noremap = true })
     end,
   },
-  window = {
-    completion = cmp.config.window.bordered(),
-    documentation = cmp.config.window.bordered(),
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      local null_ls = require("null-ls")
+
+      opts = {
+        on_attach = on_attach,
+        sources = {
+          null_ls.builtins.completion.spell,
+          null_ls.builtins.formatting.stylua,
+        },
+      }
+      null_ls.setup(opts)
+    end,
   },
-  mapping = cmp.mapping.preset.insert({
-    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-    ["<C-f>"] = cmp.mapping.scroll_docs(4),
-    ["<C-Space>"] = cmp.mapping.complete(),
-    ["<C-e>"] = cmp.mapping.abort(),
-    ["<CR>"] = cmp.mapping.confirm({ select = true }),
-    ["<Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-        -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
-        -- they way you will only jump inside the snippet region
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
-      elseif has_words_before() then
-        cmp.complete()
-      else
-        fallback()
-      end
-    end, { "i", "s" }),
-
-    ["<S-Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end, { "i", "s" }),
-  }),
-  sources = cmp.config.sources({
-    { name = "nvim_lsp" },
-    { name = "luasnip" },
-  }, {
-    { name = "buffer" },
-  }),
-})
-
-cmp.setup.filetype("gitcommit", {
-  sources = cmp.config.sources({
-    { name = "cmp_git" },
-  }, {
-    { name = "buffer" },
-  }),
-})
-
-cmp.setup.cmdline({ "/", "?" }, {
-  mapping = cmp.mapping.preset.cmdline(),
-  sources = {
-    { name = "buffer" },
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    opts = {
+      show_current_context = true,
+      show_current_context_start = true,
+    },
   },
-})
-
-cmp.setup.cmdline(":", {
-  mapping = cmp.mapping.preset.cmdline(),
-  sources = cmp.config.sources({
-    { name = "path" },
-  }, {
-    { name = "cmdline" },
-  }),
-})
-
--- Telescope
-require("telescope").load_extension("fzf")
-
-local telescope_builtin = require("telescope.builtin")
-vim.keymap.set("n", "<leader>ff", telescope_builtin.find_files, {})
-vim.keymap.set("n", "<leader>fg", telescope_builtin.live_grep, {})
-vim.keymap.set("n", "<leader>fb", telescope_builtin.buffers, {})
-vim.keymap.set("n", "<leader>fh", telescope_builtin.help_tags, {})
-vim.keymap.set("n", "<leader>fc", telescope_builtin.current_buffer_fuzzy_find, {})
-
--- Trouble
-require("trouble").setup({})
-
-local lsp_signs = {
-  Error = " ",
-  Warn = " ",
-  Hint = " ",
-  Info = " ",
-  Other = "﫠",
+  "numToStr/Comment.nvim",
+  "lewis6991/gitsigns.nvim",
+  "windwp/nvim-autopairs",
+  {
+    "nvim-tree/nvim-tree.lua",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    version = "nightly",
+  },
+  {
+    "glepnir/dashboard-nvim",
+    event = "VimEnter",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+  },
+  {
+    "utilyre/barbecue.nvim",
+    version = "*",
+    dependencies = {
+      "SmiteshP/nvim-navic",
+      "nvim-tree/nvim-web-devicons",
+    },
+    opts = {
+      theme = "tokyonight",
+    },
+  },
+  {
+    "akinsho/bufferline.nvim",
+    version = "v3.*",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+  },
+  {
+    "ggandor/leap.nvim",
+    dependencies = "tpope/vim-repeat",
+    config = function()
+      require("leap").add_default_mappings()
+    end,
+  },
 }
 
-for type, icon in pairs(lsp_signs) do
-  local hl = "DiagnosticSign" .. type
-  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-end
-
-vim.keymap.set("n", "<leader>xx", "<cmd>TroubleToggle<cr>", { silent = true, noremap = true })
-vim.keymap.set("n", "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<cr>", { silent = true, noremap = true })
-vim.keymap.set("n", "<leader>xd", "<cmd>TroubleToggle document_diagnostics<cr>", { silent = true, noremap = true })
-vim.keymap.set("n", "<leader>xl", "<cmd>TroubleToggle loclist<cr>", { silent = true, noremap = true })
-vim.keymap.set("n", "<leader>xq", "<cmd>TroubleToggle quickfix<cr>", { silent = true, noremap = true })
-vim.keymap.set("n", "gR", "<cmd>TroubleToggle lsp_references<cr>", { silent = true, noremap = true })
-
--- Colors
-vim.cmd.colorscheme("tokyonight-night")
-
--- Statusline
-require("lualine").setup({
-  options = {
-    theme = "tokyonight",
-  },
-})
-
--- Null LS
-local null_ls = require("null-ls")
-
-null_ls.setup({
-  on_attach = on_attach,
-  sources = {
-    null_ls.builtins.completion.spell,
-    null_ls.builtins.formatting.stylua,
-  },
-})
-
--- Ident Blankline
-require("indent_blankline").setup({
-  show_current_context = true,
-  show_current_context_start = true,
-})
-
--- Barbecue
-require("barbecue").setup({
-  theme = "tokyonight",
-})
+require("lazy").setup(plugins)

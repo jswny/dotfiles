9832b5f5b5313504fbc9ac10bbbbf485a2dc591d
Joe Sweeney <joe@j1.io>
2023-12-01 20:17:55 -0500

    Add prettier formatting

--- neovim/init.lua
+++ neovim/init.lua
@@ -286,6 +286,7 @@ local plugins = {
         sources = {
           null_ls.builtins.completion.spell,
           null_ls.builtins.formatting.stylua,
+          null_ls.builtins.formatting.prettier,
         },
       }
     end,

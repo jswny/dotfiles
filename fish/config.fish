27c0fecb6fd35fd4ea17d19def32fa9f169c4f52
Joe Sweeney <joe@j1.io>
2025-09-27 12:38:56 -0400

    Add atuin

--- fish/config.fish
+++ fish/config.fish
@@ -10,17 +10,22 @@ alias gp "git pull"
 alias gpp "git push"
 alias gca "git add . && git commit --amend --no-edit && git push -f"
 
+fish_add_path ~/.orbstack/bin
+fish_add_path ~/.cargo/bin
+fish_add_path ~/.bun/bin
+
+source ~/.atuin/bin/env.fish
 if status is-interactive
     set -gx EDITOR "zed --wait"
 
     function rg
         command rg --json -C 2 $argv | delta
     end
+
+    source ~/.atuin/bin/env.fish
+    atuin init fish | source
 end
 
-fish_add_path ~/.orbstack/bin
-fish_add_path ~/.cargo/bin
-fish_add_path ~/.bun/bin
 
 set -gx BAT_THEME "ansi"
 set -gx FZF_DEFAULT_OPTS "--color=16"

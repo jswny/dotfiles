0bee0922ca131ff2ca33ecbb619c12c614785f7b
Joe Sweeney <joe@j1.io>
2025-09-27 13:13:02 -0400

    Add mise

--- fish/config.fish
+++ fish/config.fish
@@ -1,20 +1,24 @@
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
 alias ll "ls -la"
-alias cat "bat"
+alias cat bat
 
 alias gs "git status"
+alias gc "git commit -m"
 alias gl "git log"
 alias gd "git diff"
 alias gp "git pull"
+alias gaa "git add ."
 alias gpp "git push"
 alias gca "git add . && git commit --amend --no-edit && git push -f"
+
+mise activate fish | source
+source ~/.atuin/bin/env.fish
 
 fish_add_path ~/.orbstack/bin
 fish_add_path ~/.cargo/bin
 fish_add_path ~/.bun/bin
 
-source ~/.atuin/bin/env.fish
 if status is-interactive
     set -gx EDITOR "zed --wait"
 
@@ -22,12 +26,10 @@ if status is-interactive
         command rg --json -C 2 $argv | delta
     end
 
-    source ~/.atuin/bin/env.fish
     atuin init fish | source
 end
 
-
-set -gx BAT_THEME "ansi"
+set -gx BAT_THEME ansi
 set -gx FZF_DEFAULT_OPTS "--color=16"
 
 source ~/.config/fish/local.config.fish
